f07f02bf988343e372aacf65665b8e1fbc737bc6
Davide Di Pumpo <davide.dipumpo@credimi.com>
2020-08-25 21:05:46 +0200

    installs nextjs

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Hello World... Almost</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">
+          Hello World... Almost!
+        </h1>
+      </main>
+
+    </div>
+  )
+}
