b97727e6e2ed8d4118a4fffc5fceeccd83a176d4
Davide Di Pumpo <davide.dipumpo@credimi.com>
2020-08-29 18:00:34 +0200

    minor bcg changes

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,5 +1,7 @@
 export const colors = {
   main: {
+    low: "#70EEDB",
+    neutral: "#28EBCD",
     high: "#4eacc5"
   }
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,76 @@
 import 'modern-css-reset/dist/reset.min.css'
 import styled from '@emotion/styled'
 import {colors} from '../constants/colors'
+import {keyframes} from '@emotion/core'
+
+const intro = keyframes`
+  from, 50% {
+    min-height: 0vh;
+    height: 0vh;
+    overflow: hidden;
+    opacity: 0;
+  }
+
+  to {
+    min-height: 100vh;
+    height: 100vh;
+  }
+
+  70% {
+    min-height: 100vh;
+    height: 100vh;
+    opacity: 1;
+  }
+
+  75%,78% {
+    min-height: 80vh;
+    height: 80vh;
+  }
+
+  80% {
+    min-height: 100vh;
+    height: 100vh;
+  }
+
+  85%,86% {
+    min-height: 90vh;
+    height: 90vh;
+  }
+
+  90% {
+    min-height: 100vh;
+    height: 100vh;
+  }
+
+  95%,96% {
+    min-height: 95vh;
+    height: 95vh;
+  }
+`
 
 const Container = styled.div`
-    background-color: ${colors.main.high};
-    color: black;
-    font-family: sans-serif;
-    min-height: 100vh;
-    padding-top: 20vh;
-    text-align: center;
-    width: 100vw;
+  animation: ${intro} 2s ease-out;
+  height: 0vh;
+  background-color: ${colors.main.low};
+  color: black;
+  position: relative;
+  font-family: sans-serif;
+  min-height: 100vh;
+  text-align: center;
+  width: 100vw;
+  z-index: 0;
+
+  &::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    background: linear-gradient(90deg, #161122 21px, transparent 1%) center, linear-gradient(#161122 21px, transparent 1%) center, transparent;
+    background-size: 26px 26px;
+  }
 `
 
 // This default export is required in a new `pages/_app.js` file.
