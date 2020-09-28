617cf933a922dcb560732fc97828f0240e19eca1
Davide Di Pumpo <davide.dipumpo@credimi.com>
2020-09-29 00:57:53 +0200

    test notifications

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -1,25 +1,41 @@
 import OneSignal from 'react-onesignal'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { Button } from './Button'
 
 export const Notification = () => {
+  const [initialized, initialize] = useState(false)
+  const [hasPush, setPush] = useState(false)
+  const [isPushEnabled, setPushEnabled] = useState(false)
+
   useEffect(() => {
-    if (typeof window !== 'undefined' && window.document) {
-      OneSignal.initialize(process.env.onsignalKey, {
-        notifyButton: {
-          enable: true,
-        },
-        subdomainName: 'piccologuaioap',
-        safari_web_id:
-          'web.onesignal.auto.28be2e0f-48c0-43fd-877a-95cc3c6fc0d9',
-      })
+    const init = async () => {
+      try {
+        if (typeof window !== 'undefined' && window.document) {
+          await OneSignal.initialize(process.env.onsignalKey, {
+            notifyButton: {
+              enable: false,
+            },
+            subdomainName: 'piccologuaioap',
+            safari_web_id:
+              'web.onesignal.auto.28be2e0f-48c0-43fd-877a-95cc3c6fc0d9',
+            allowLocalhostAsSecureOrigin: true,
+          })
+        }
+        initialize(true)
+        const hasPush = await OneSignal.isPushNotificationsSupported()
+        setPush(hasPush)
+        const isPushEnabled = await OneSignal.isPushNotificationsEnabled()
+        setPushEnabled(isPushEnabled)
+      } catch (e) {
+        console.error(e)
+      }
     }
+    init()
   }, [])
 
-  if (!OneSignal.isPushNotificationsSupported()) return null
   return (
     <Button onClick={OneSignal.registerForPushNotifications}>
-      Ricevi notifiche!
+      {isPushEnabled ? 'Notifiche attivate' : 'Ricevi notifiche!'}
     </Button>
   )
 }
