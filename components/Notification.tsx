08c10ae661a66f1c807f81db6ed299b1753096f7
Davide Di Pumpo <davide.dipumpo@credimi.com>
2020-09-29 01:38:24 +0200

    uses hook to see if onesignal is setupped

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -1,35 +1,33 @@
-import OneSignal from 'react-onesignal'
+import OneSignal, { useOneSignalSetup } from 'react-onesignal'
 import { useEffect, useState } from 'react'
 import { Button } from './Button'
 
 export const Notification = () => {
-  const [initialized, initialize] = useState(false)
   const [hasPush, setPush] = useState(false)
   const [isPushEnabled, setPushEnabled] = useState(false)
 
   useEffect(() => {
-    const init = async () => {
-      try {
-        if (typeof window !== 'undefined' && window.document) {
-          await OneSignal.initialize(process.env.onsignalKey, {
-            subdomainName: 'piccologuaioap',
-            safari_web_id:
-              'web.onesignal.auto.28be2e0f-48c0-43fd-877a-95cc3c6fc0d9',
-            allowLocalhostAsSecureOrigin: true,
-          })
-        }
-        initialize(true)
-        const hasPush = await OneSignal.isPushNotificationsSupported()
-        setPush(hasPush)
-        const isPushEnabled = await OneSignal.isPushNotificationsEnabled()
-        setPushEnabled(isPushEnabled)
-      } catch (e) {
-        console.error(e)
-      }
+    if (typeof window !== 'undefined' && window.document) {
+      OneSignal.initialize(process.env.onsignalKey, {
+        subdomainName: 'piccologuaioap',
+        safari_web_id:
+          'web.onesignal.auto.28be2e0f-48c0-43fd-877a-95cc3c6fc0d9',
+        allowLocalhostAsSecureOrigin: true,
+      })
     }
-    init()
   }, [])
 
+  useOneSignalSetup(() => {
+    const init = async () => {
+      const hasPush = await OneSignal.isPushNotificationsSupported()
+      setPush(hasPush)
+      const isPushEnabled = await OneSignal.isPushNotificationsEnabled()
+      setPushEnabled(isPushEnabled)
+    }
+    init()
+  })
+
+  if (!hasPush) return null
   return (
     <Button onClick={OneSignal.registerForPushNotifications}>
       {isPushEnabled ? 'Notifiche attivate' : 'Ricevi notifiche!'}
