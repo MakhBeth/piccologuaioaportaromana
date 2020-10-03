917883818a3044ae501e24d9c6de1c277637035a
Davide Di Pumpo <davide.dipumpo@credimi.com>
2020-10-04 01:53:33 +0200

    adds short name

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,29 @@ const Home = () => {
     <div className="container">
       <Head>
         <title>Hello World... Almost</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="apple-mobile-web-app-title" content="Gaia" />
+        <meta name="application-name" content="Gaia" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <link
           href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap"
           rel="stylesheet"
