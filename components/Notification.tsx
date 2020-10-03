40cce8e1b80cdfa5bcafbdbd1310ae87f6ebf3ab
Davide Di Pumpo <davide.dipumpo@credimi.com>
2020-10-04 01:31:45 +0200

    adds animations

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -16,10 +16,21 @@ import { useInterval } from '../hooks/useInterval'
 import { useIsMounted } from '../hooks/useIsMounted'
 import Modal from 'react-modal'
 import { opacify } from 'polished'
-import { jsx, ClassNames } from '@emotion/core'
+import { jsx, ClassNames, keyframes } from '@emotion/core'
 import { colors } from '../constants/colors'
 import serialize from 'form-serialize'
 Modal.setAppElement('#__next')
+
+const animation = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0.8);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+`
 
 const OneSignalComponent: React.FunctionComponent<{
   setPermission: Dispatch<SetStateAction<NotificationPermission>>
@@ -53,15 +64,24 @@ const Mail: React.FunctionComponent = () => {
   const openModal = () => setModal(true)
   const closeModal = () => setModal(false)
   const { t } = useTranslation()
+  const [loading, setLoading] = useState<boolean | null>(null)
   const submit = useCallback(e => {
-    e.preventDefault()
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: serialize(e.target),
-    })
-      .then(() => console.log('Success!'))
-      .catch(error => console.error(error))
+    if (e.target.checkValidity()) {
+      setLoading(true)
+      e.preventDefault()
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: serialize(e.target),
+      })
+        .then(() => {
+          setLoading(false)
+        })
+        .catch(error => {
+          console.error(error)
+          setLoading(null)
+        })
+    }
   }, [])
 
   return (
@@ -82,25 +102,75 @@ const Mail: React.FunctionComponent = () => {
               align-items: center;
             `}
             css={css`
-              background: ${colors.neutral.highest};
+              animation: 300ms ${animation};
+              font-size: 1.2rem;
+              background: ${colors.main.neutral};
               margin: 1rem 1rem 2rem;
               max-width: calc(50rem);
               padding: 1rem 2rem;
               border-radius: 1rem;
               width: 100%;
               outline: none;
+              text-align: center;
+              position: relative;
             `}
             isOpen={modal}
             onRequestClose={closeModal}
           >
-            <h1>Il tuo browser non supporta le notifiche!</h1>
-            <h3>Oppure ha un ad-block di quelli potenti!</h3>
-            <br />
-            <form name="notifications" data-netlify="true" onSubmit={submit}>
-              <label htmlFor="email">Prova a lasciarci un'email</label>
-              <input type="hidden" name="form-name" value="notifications" />
-              <input type="email" name="email" id="email" />
-            </form>
+            {loading !== null && (
+              <div
+                css={css`
+                  position: absolute;
+                  width: 100%;
+                  height: 100%;
+                  top: 0;
+                  left: 0;
+                  background-color: ${colors.main.neutral};
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  z-index: 1;
+                `}
+              >
+                {loading ? (
+                  t('loading')
+                ) : (
+                  <div>
+                    <h1>Perfetto!</h1>
+                    <Button onClick={closeModal}>Chiudi</Button>
+                  </div>
+                )}
+              </div>
+            )}
+            <Fragment>
+              <h1>Il tuo browser non supporta le notifiche!</h1>
+              <h3>Oppure ha un ad-block di quelli potenti!</h3>
+              <br />
+              <form name="notifications" data-netlify="true" onSubmit={submit}>
+                <label htmlFor="email">Prova a lasciarci un'email</label>
+                <br />
+                <input type="hidden" name="form-name" value="notifications" />
+                <input
+                  css={css`
+                    border: 2px solid transparent;
+                    padding: 0.5rem 1rem;
+                    min-width: 300px;
+                    &:focus {
+                      border: 2px solid ${colors.main.low};
+                      outline: none;
+                    }
+                  `}
+                  type="email"
+                  name="email"
+                  id="email"
+                  required
+                />
+                <br />
+                <br />
+                <Button type="submit">Invia</Button>
+                <Button onClick={closeModal}>Chiudi</Button>
+              </form>
+            </Fragment>
           </Modal>
         )}
       </ClassNames>
@@ -124,7 +194,6 @@ const Notifications = ({ t }: { readonly t: TFunction }) => {
           setPermission(Notification.permission)
         })
         .catch(e => {
-          console.log(e)
           console.warn(e, 'onesignal blocked')
         })
     }
