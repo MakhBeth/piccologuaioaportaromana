360759ecf4f625cf3a9edad69d4dd958c0579a25
Davide Di Pumpo <davide.dipumpo@credimi.com>
2021-01-06 22:09:15 +0100

    removes almost if born

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,18 +5,19 @@ import App from 'next/app'
 import { appWithTranslation } from '../i18n'
 import { Container } from '../components/Container'
 import { Heart } from '../components/Heart'
+import { bornValues } from '../constants/bornValues'
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }) {
   return (
     <Container>
       <NextSeo
-        title="Hello World... almost!"
+        title={`Hello World${bornValues.born ? '' : '... almost'}!`}
         description="I miei genitori hanno messo su \'sta roba. Dategli un occhio, fateli felici"
         canonical="https://piccologuaioaportaromana.com/"
         openGraph={{
           url: 'https://piccologuaioaportaromana.com/',
-          title: 'Hello World... almost!',
+          title: `Hello World${bornValues.born ? '' : '... almost'}!`,
           description:
             "I miei genitori hanno messo su 'sta roba. Dategli un occhio, fateli felici",
           images: [{ url: 'https://piccologuaioaportaromana.com/preview.jpg' }],
